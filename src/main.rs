d5308eafce3ccc467bd77eaf97cb03ba14ee92cb
Alex Bondar <abondar1992@gmail.com>
2024-10-28 16:50:19 +0100

    enums

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use crate::Message::{Quit, Start, Write};
+
 fn main() {
     println!("Let's start the show");
 
     //todo add arg reading and calling functions by args
+    //todo move functions to mods
     base_var();
     mutable_var();
     scope();
@@ -21,7 +24,8 @@ fn main() {
     borrowing();
     strings();
     arrays();
-    structs()
+    structs();
+    enums()
 }
 
 fn base_var() {
@@ -298,7 +302,6 @@ fn structs() {
 
 }
 
-
 //tuple struct
 struct Point(i32,i32,i32);
 
@@ -306,4 +309,67 @@ struct Point(i32,i32,i32);
 struct User {
     name: String,
     pass: String,
+}
+
+fn enums(){
+    println!("enums");
+
+    println!("{:?}", Number::One);
+    println!("{:?}", Number::One as u16);
+    println!("{:?}", Num::One);
+
+    let msg_quit: Message = Quit(String::from("Reason"), String::from("Epic"));
+    let msg_start: Message = Start {x:0,y:32};
+    let msg_write: Message = Write(String::from("Hiii"));
+
+    if let Start {x:a,y:b}= msg_start {
+        println!("{}", a)
+    }
+
+    if let Write(ref message) = msg_write{
+        println!("{}", message);
+    }
+
+    let msgs: [Message;3] = [
+        msg_quit,
+        msg_start,
+        msg_write
+    ];
+
+    for msg in msgs {
+        println!("{:?}", msg)
+    }
+
+    let mut  nm: Option<i32> = Some(5);
+    nm = nm.map(|v| v+1);
+
+    match nm {
+        Some(v)=> println!("{}", v),
+        None => println!("no val")
+    }
+
+}
+
+
+#[derive(Debug)]
+enum Number {
+    Zero,
+    One,
+    Two
+}
+
+#[derive(Debug)]
+enum Num {
+    Zero = 0,
+    One =1
+}
+
+#[derive(Debug)]
+enum Message {
+    Quit(String,String),
+    Start{
+        x: i32,
+        y: i32
+    },
+    Write(String)
 }
