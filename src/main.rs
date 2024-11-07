1bfd3b2bcf9224c58512e6ac54db1159543c631d
Alex Bondar <abondar1992@gmail.com>
2024-11-07 17:19:48 +0100

    closure

--- src/main.rs
+++ src/main.rs
@@ -42,9 +42,9 @@ fn main() {
     hashmaps();
     type_coerc();
     res(false);
-    lifetime()
+    lifetime();
+    closure()
 }
-
 
 
 fn scope() {
@@ -684,4 +684,44 @@ fn compare<'a>(x: &'a i32, y: &'a i32) -> &'a i32 {
     } else {
         y
     }
+}
+
+fn closure(){
+    println!("closure");
+
+    let incr = |i: i32| -> i32 { i + 1};
+    println!("Incr: {}",incr(7));
+
+    let val = ||9;
+    println!("{}",val());
+
+    let name = String::from("Arman");
+
+    //greet will own name
+    let greet = move || println!("Hello {}",name);
+    greet();
+
+    let mut count = 0;
+    let mut cnt = move || {
+         count +=1;
+        println!("{}", count)
+     };
+    cnt();
+    println!("Orig count: {}",count);
+
+    let _reborrow: &i32 = &count;
+    cnt();
+
+    let closure_param = ||println!("Closure passed as a param");
+    func_receiver(fn_param);
+    func_receiver(closure_param);
+}
+
+fn func_receiver<F: Fn()>(f: F){
+    f();
+}
+
+
+fn fn_param(){
+    println!("Function passed as a param")
 }
