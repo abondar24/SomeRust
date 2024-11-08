b9ea77429927392539631e7ae2484f67d49a1d0d
Alex Bondar <abondar1992@gmail.com>
2024-11-08 16:34:02 +0100

    extended argument reading

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,18 @@ mod result;
 mod types;
 
 use crate::base::{base_var, expr, lp, mutable_var};
+use crate::memory::{assignments, borrowing, lifetime, ownership, rebind, scope, shadow};
+use crate::types::{bool, char, float, range, strings, tuples, unit, unsigned};
+use crate::functions::{func,closure};
+
 use std::collections::HashMap;
 use std::env;
 use std::fmt::Display;
 use std::hash::Hash;
+use crate::coercion::type_coerc;
+use crate::cust_type::{enums, generics, pattern, structs, traits};
+use crate::data_structs::{arrays, hashmaps, iterator, vectors};
+use crate::result::res;
 
 fn main() {
     let arguments = init_arguments();
@@ -27,7 +35,34 @@ fn main() {
                 "mv" => mutable_var(),
                 "expr" => expr(),
                 "lp" => lp(),
-
+                "sc" => scope(),
+                "sh" => shadow(),
+                "reb" => rebind(),
+                "asn" => assignments(),
+                "own" => ownership(),
+                "bor" => borrowing(),
+                "lft" => lifetime(),
+                "tup" => tuples(),
+                "uns" => unsigned(),
+                "flt" => float(),
+                "rng" => range(),
+                "chr" => char(),
+                "str" => strings(),
+                "bool" => bool(),
+                "unit" => unit(),
+                "fnc" => call_panic_func(args,"fnc"),
+                "cls" => closure(),
+                "arr" => arrays(),
+                "vec" => vectors(),
+                "map" => hashmaps(),
+                "itr" => iterator(),
+                "enm" => enums(),
+                "strc" => structs(),
+                "ptn" => pattern(),
+                "trt" => traits(),
+                "gen" => generics(),
+                "crc" => type_coerc(),
+                "res" => call_panic_func(args,"res"),
                 _ => println!("Unsupported action"),
             }
         } else if arg_val == "help" {
@@ -39,43 +74,7 @@ fn main() {
         println!("Please provide an argument! Use cargo run help to see the available commands.");
     }
 
-    //todo add arg reading and calling functions by args
     //todo readme
-
-    // memory::scope();
-    // memory::shadow();
-    // memory::rebind();
-    // memory::assignments();
-    // memory::ownership();
-    // memory::borrowing();
-    // memory::lifetime();
-    //
-    // types::tuples();
-    // types::unsigned();
-    // types::float();
-    // types::range();
-    // types::char();
-    // types::strings();
-    // types::bool();
-    // types::unit();
-    //
-    // functions::func(false);
-    // functions::closure();
-    //
-    // data_structs::arrays();
-    // data_structs::vectors();
-    // data_structs::hashmaps();
-    // data_structs::iterator();
-    //
-    // cust_type::enums();
-    // cust_type::structs();
-    // cust_type::pattern();
-    // cust_type::traits();
-    // cust_type::generics();
-    //
-    // coercion::type_coerc();
-    //
-    // result::res(false);
 }
 
 fn init_arguments() -> HashMap<String, String> {
@@ -84,13 +83,65 @@ fn init_arguments() -> HashMap<String, String> {
     arguments.insert("mv".to_string(), "Mutable Variables".to_string());
     arguments.insert("expr".to_string(), "Expressions".to_string());
     arguments.insert("lp".to_string(), "Loop".to_string());
+    arguments.insert("sc".to_string(), "Scopes of variables".to_string());
+    arguments.insert("sh".to_string(), "Shadowing".to_string());
+    arguments.insert("reb".to_string(), "Rebinding".to_string());
+    arguments.insert("asn".to_string(), "Assignments destructuring".to_string());
+    arguments.insert("own".to_string(), "Ownership of variables".to_string());
+    arguments.insert("bor".to_string(), "Borrowing of variable values".to_string());
+    arguments.insert("lft".to_string(), "Lifetimes usage in Rust".to_string());
+    arguments.insert("tup".to_string(), "Tuple example".to_string());
+    arguments.insert("uns".to_string(), "Unsigned types demo".to_string());
+    arguments.insert("flt".to_string(), "Float type example".to_string());
+    arguments.insert("rng".to_string(), "Ranges in Rust".to_string());
+    arguments.insert("chr".to_string(), "Character usage".to_string());
+    arguments.insert("str".to_string(), "Strings example".to_string());
+    arguments.insert("bool".to_string(), "Booleans example".to_string());
+    arguments.insert("unit".to_string(), "Unit type usage".to_string());
+    arguments.insert("fnc".to_string(),"Functions usage. Requires additional argument true or false to call panic".to_string());
+    arguments.insert("cls".to_string(), "Closures examples in rust".to_string());
+    arguments.insert("arr".to_string(), "Arrays example".to_string());
+    arguments.insert("vec".to_string(), "Vectors example".to_string());
+    arguments.insert("map".to_string(), "Maps example".to_string());
+    arguments.insert("itr".to_string(), "Iterators example".to_string());
+    arguments.insert("enm".to_string(), "Enums example".to_string());
+    arguments.insert("strc".to_string(), "Structs and associated functions example".to_string());
+    arguments.insert("ptn".to_string(), "Pattern check example".to_string());
+    arguments.insert("trt".to_string(), "Traits example".to_string());
+    arguments.insert("gen".to_string(), "Generics example".to_string());
+    arguments.insert("crc".to_string(), "Type coercion example".to_string());
+
 
     arguments
 }
 
 fn show_help(arguments: &HashMap<String, String>) {
-    println!("Valid arguments:");
+    println!("List of valid commands:");
     for (arg, description) in arguments {
         println!("  {} -> {}", arg, description);
+    }
+}
+
+fn call_panic_func(args: Vec<String>,fnc: &str) {
+    let arg2 = args.get(2);
+    let is_panic = parse_bool(arg2);
+
+    match parse_bool(arg2) {
+        Ok(parsed_value) => println!("Parsed value: {}", parsed_value),
+        Err(e) => println!("{}", e),
+    }
+
+    match fnc {
+        "fnc" => func(is_panic.unwrap()),
+        "res"=>  res(is_panic.unwrap()),
+        _=> panic!("Unknown function {}", fnc)
+    }
+
+}
+
+fn parse_bool(input: Option<&String>) -> Result<bool, String> {
+    match input {
+        Some(value) => value.parse::<bool>().map_err(|_| "Failed to parse the string into a bool.".to_string()),
+        None => Err("Not enough arguments.".to_string()),
     }
 }
